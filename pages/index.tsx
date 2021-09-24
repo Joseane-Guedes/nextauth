a61ee69f2b85ad7d4971bdb03f9b6619ee0364ba
Joseane-Guedes <joseane_23@hotmail.com>
2021-09-24 20:02:51 -0300

    Axios: intercept failure request queue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import {  FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-//import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
