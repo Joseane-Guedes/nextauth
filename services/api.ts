c8009e3771a6f5a7e6befb4822afd4b00b4a962a
Joseane-Guedes <joseane_23@hotmail.com>
2021-09-26 15:42:39 -0300

    add refresh token feature

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies';
+import axios, { AxiosError } from 'axios';
+import { parseCookies, setCookie } from 'nookies';
 
 
 let cookies = parseCookies();
@@ -10,3 +10,36 @@ export const api = axios.create ({
         Authorization: `Bearer ${cookies['nextauth.token']}`
     }
 });
+
+api.interceptors.response.use(response => {
+    return response; //success
+  }, (error: AxiosError) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === 'token.expired') {
+        // refresh token
+        cookies = parseCookies();
+  
+        const { 'nextauth.refreshToken': refreshToken } = cookies;
+  
+        api.post('/refresh', {
+          refreshToken,
+        }).then(response => {
+          const { token } = response.data;
+  
+          setCookie(undefined, 'nextauth.token', token, {
+            maxAge: 60 * 60 * 24 * 30, //30 days
+            path: '/' //all routes can access this cookie
+          });
+  
+          setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+            maxAge: 60 * 60 * 24 * 30, //30 days
+            path: '/' //all routes can access this cookie
+          });
+  
+          api.defaults.headers['Authorization'] = `Bearer ${token}`; //updating token after login
+        })
+      } else {
+        // logout user
+      }
+    }
+  })
